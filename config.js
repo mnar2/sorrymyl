7d76b3e21b73675a73c4bb426443f7424451eff7
mnar2 <mmyacc45@gmail.com>
2024-08-08 00:22:52 +0100

    Update config.js

--- config.js
+++ config.js
@@ -10,13 +10,5 @@ var config = {
         "I feel helpless, but I want you to know I'm here for you,",
         "always。。。",
     ],
-    // 相爱的时间 记住格式不能写错了, 非常重要
-    // 格式一定要和下面这个时间格式一样
-    // 分别是 年-月-日T时时:分分:秒秒
-    //      温馨警告:
-    //          1.时分秒都必须是俩个数字  比如 00:00:00
-    //          2.T不能丢了
-    date: "2017-07-20T20:15:00",
-    // 双方的名字
     names: ["i hope", "this helps"]
 }
